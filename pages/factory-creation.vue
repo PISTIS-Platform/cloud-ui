98ae008332f4458fdd8ceb99fab1044e6e26e20d
Hermes J. Pappas <hjp@hjp.gr>
2024-06-19 09:11:49 +0300

    Make factory prefix input and bring others closer

--- pages/factory-creation.vue
+++ pages/factory-creation.vue
@@ -36,29 +36,72 @@ const onSubmit = () => {
                 <template #header>
                     <SubHeading :title="t('registry.createFactory')" />
                 </template>
-                <div class="flex flex-col gap-4">
-                    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-                        <UFormGroup :label="t('registry.organizationName')" name="organizationName" required>
-                            <UInput v-model="state.organizationName" />
-                        </UFormGroup>
-                        <!--TODO: Will the ID be uuid v4?-->
-                        <UFormGroup :label="t('registry.organizationId')" name="organizationId" required>
-                            <UInput v-model="state.organizationId" />
-                        </UFormGroup>
-                        <UFormGroup :label="t('registry.country')" name="country" required>
-                            <UInput v-model="state.country" />
-                        </UFormGroup>
-                        <UFormGroup :label="t('registry.factoryPrefix')" name="factoryPrefix" required>
-                            <UInput v-model="state.factoryPrefix" />
-                        </UFormGroup>
-                        <UFormGroup :label="t('registry.factoryAccepted')" name="isAccepted">
-                            <UToggle v-model="state.isAccepted" />
-                        </UFormGroup>
-                        <UFormGroup :label="t('registry.ip')" name="ip">
-                            <UInput v-model="state.ip" />
-                        </UFormGroup>
+                <div class="flex w-full">
+                    <UForm :schema="schema" :state="state" class="space-y-4 w-full flex flex-col" @submit="onSubmit">
+                        <div class="flex w-full items-center gap-4">
+                            <UFormGroup
+                                :label="t('registry.organizationName')"
+                                name="organizationName"
+                                required
+                                class="w-full"
+                            >
+                                <UInput v-model="state.organizationName" />
+                            </UFormGroup>
+                            <!--TODO: Will the ID be uuid v4?-->
+                            <UFormGroup
+                                :label="t('registry.organizationId')"
+                                name="organizationId"
+                                required
+                                class="w-full"
+                            >
+                                <UInput v-model="state.organizationId" />
+                            </UFormGroup>
+                        </div>
+                        <div class="flex w-full items-center gap-4">
+                            <UFormGroup :label="t('registry.country')" name="country" required class="w-full">
+                                <UInput v-model="state.country" />
+                            </UFormGroup>
+                            <UFormGroup
+                                :label="t('registry.factoryPrefix')"
+                                name="factoryPrefix"
+                                required
+                                class="w-full"
+                            >
+                                <div class="w-full flex items-center">
+                                    <UButton
+                                        color="white"
+                                        :ui="{ color: { white: { solid: 'hover:bg-white' } } }"
+                                        class="text-gray-400 cursor-default hover:none rounded-l-lg rounded-r-none"
+                                        >https://</UButton
+                                    >
+                                    <UInput
+                                        v-model="state.factoryPrefix"
+                                        :ui="{ rounded: 'rounded-none' }"
+                                        class="w-full"
+                                    />
+                                    <UButton
+                                        color="white"
+                                        :ui="{ color: { white: { solid: 'hover:bg-white' } } }"
+                                        class="text-gray-400 cursor-default rounded-l-none rounded-r-lg"
+                                        >.pistis-project.eu</UButton
+                                    >
+                                </div>
+                            </UFormGroup>
+                        </div>
+                        <div class="w-1/2 items-center flex gap-4">
+                            <UFormGroup :label="t('registry.ip')" name="ip" class="w-full">
+                                <UInput v-model="state.ip" />
+                            </UFormGroup>
+                            <UFormGroup :label="t('registry.factoryAccepted')" name="isAccepted" class="w-44">
+                                <div class="w-full items-center justify-center flex h-full">
+                                    <UToggle v-model="state.isAccepted" class="-ml-3" />
+                                </div>
+                            </UFormGroup>
+                        </div>
 
-                        <UButton type="submit" :disabled="!schema.safeParse(state).success">Create Factory</UButton>
+                        <UButton type="submit" :disabled="!schema.safeParse(state).success" class="w-[120px]"
+                            >Create Factory</UButton
+                        >
                     </UForm>
                 </div>
             </UCard>
