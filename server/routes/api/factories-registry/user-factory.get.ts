3e6eec5759f12c0510882bdd1e4e583639dc1b9e
Hermes J. Pappas <hjp@hjp.gr>
2024-07-05 11:51:32 +0300

    Remove dummy response

--- server/routes/api/factories-registry/user-factory.get.ts
+++ server/routes/api/factories-registry/user-factory.get.ts
@@ -5,29 +5,9 @@ const { factoryRegistryURL } = useRuntimeConfig();
 export default defineEventHandler(async (event) => {
     const token = await getToken({ event });
 
-    const dummyResponse = {
-        id: '768004e7-33b1-4248-bafe-04688f49f161',
-        organizationName: 'TestOrg',
-        organizationId: '8aff8e9b-1322-4395-a53e-c445d159eb80',
-        factoryPrefix: 'test-prefix',
-        ip: '192.168.1.1',
-        // ip: null,
-        country: 'Greece',
-        status: 'live',
-        isAccepted: false,
-        isActive: false,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    };
-
-    console.log(token);
-    console.log(factoryRegistryURL);
-
-    // const response = await $fetch(`${factoryRegistryURL}/factories/user-factory`, {
-    //     headers: {
-    //         Authorization: `Bearer ${token?.access_token}`,
-    //     },
-    // });
-
-    return dummyResponse;
+    return $fetch(`${factoryRegistryURL}/factories/user-factory`, {
+        headers: {
+            Authorization: `Bearer ${token?.access_token}`,
+        },
+    });
 });
