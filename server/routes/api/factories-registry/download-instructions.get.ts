4972e35c58b28eccf567a037065c3c18ec0581b3
Hermes J. Pappas <hjp@hjp.gr>
2024-07-05 11:54:02 +0300

    Return results directly

--- server/routes/api/factories-registry/download-instructions.get.ts
+++ server/routes/api/factories-registry/download-instructions.get.ts
@@ -5,11 +5,9 @@ const { factoryRegistryURL } = useRuntimeConfig();
 export default defineEventHandler(async (event) => {
     const token = await getToken({ event });
 
-    const response = await $fetch(`${factoryRegistryURL}/factories/download-instructions`, {
+    return $fetch(`${factoryRegistryURL}/factories/download-instructions`, {
         headers: {
             Authorization: `Bearer ${token?.access_token}`,
         },
     });
-
-    return response;
 });
