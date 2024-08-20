64d6c36202ba6f831d024636c1dc3c73c740b6bf
Hermes J. Pappas <hjp@hjp.gr>
2024-08-20 13:18:42 +0300

    Use store for semi-permanence

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -1,63 +1,24 @@
 <script setup lang="ts">
 //TODO: API call to get notifications here
 //TODO: Possibly read information from API call that will happen to get the notifications for the icon
+import dayjs from 'dayjs';
 
-const notifications = ref([
-    {
-        id: '1',
-        userId: '2',
-        organizationId: '3',
-        message: 'Your notification text goes here.',
-        isHidden: false,
-        createdAt: new Date(),
-        readAt: null,
-    },
-    {
-        id: '2',
-        userId: '2',
-        organizationId: '3',
-        message:
-            'Sit ad dolor sint. Qui laboris Lorem elit velit. Incididunt nulla nisi ipsum et aliquip nulla fugiat ullamco. Quis sit elit exercitation mollit elit ut sint. Et id dolore velit cillum. Commodo fugiat proident elit irure exercitation deserunt sint. Nulla velit consectetur sit mollit ex.',
-        isHidden: false,
-        createdAt: new Date(),
-        readAt: null,
-    },
-    {
-        id: '3',
-        userId: '2',
-        organizationId: '3',
-        message: 'Your notification text goes here.',
-        isHidden: true,
-        createdAt: new Date(),
-        readAt: null,
-    },
-    {
-        id: '4',
-        userId: '2',
-        organizationId: '3',
-        message: 'Your notification text goes here.',
-        isHidden: false,
-        createdAt: new Date(),
-        readAt: new Date(),
-    },
-]);
+import { useMessagesStore } from '~/store/messages';
+
+const messagesStore = useMessagesStore();
+
+const notifications = computed(() => messagesStore.getMessages);
 
 const markAsRead = (id: string | number) => {
-    //TODO: Send API call to mark notification as Read
-    //TODO: Refresh list upon doing so
-    const foundNotification = notifications.value.find((notification) => notification.id === id);
-    if (!foundNotification) return;
-
-    foundNotification.readAt = new Date();
+    //TODO: Send API call to mark notification as Read if stored
+    //TODO: Refresh list upon doing so and set in store
+    messagesStore.markAsRead(id);
 };
 
 const hide = (id: string | number) => {
-    //TODO: Send API call to mark notification as hidden
-    //TODO: Refresh list upon doing so
-    const foundNotification = notifications.value.find((notification) => notification.id === id);
-    if (!foundNotification) return;
-
-    foundNotification.isHidden = true;
+    //TODO: Send API call to mark notification as hidden if stored
+    //TODO: Refresh list upon doing so and set in store
+    messagesStore.hideMessage(id);
 };
 
 const shownNotifications = computed(() => notifications.value.filter((not) => !not.isHidden));
@@ -71,9 +32,12 @@ const shownNotifications = computed(() => notifications.value.filter((not) => !n
                 <UCard
                     v-for="notification in shownNotifications"
                     :key="notification.id"
-                    :class="['w-full relative pb-6', notification.readAt ? 'opacity-60' : '']"
+                    :class="['w-full relative pb-6 pt-4', notification.readAt ? 'opacity-60' : '']"
                 >
                     {{ notification.message }}
+                    <div class="flex items-center justify-end w-full text-xs absolute top-2 right-2 text-gray-500">
+                        {{ dayjs(notification.createdAt).format('DD/MM/YYYY HH:mm') }}
+                    </div>
                     <div class="w-full flex justify-end gap-4 absolute bottom-2 right-2">
                         <UButton color="red" variant="soft" @click="hide(notification.id)">{{
                             $t('notifications.hide')
