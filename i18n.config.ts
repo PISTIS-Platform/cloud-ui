079fc9757f9241b061b61e07d94ec34547f3cf70
Giannis Giannopoulos <giannis.giannopoulos@suite5.eu>
2024-09-16 16:59:06 +0300

    Add creation wallet alert

--- i18n.config.ts
+++ i18n.config.ts
@@ -49,6 +49,8 @@ export default defineI18nConfig(() => ({
             dashboard: {
                 dashboard: 'Dashboard',
                 status: 'Status',
+                wallet: 'Please activate your wallet',
+                walletButton: 'Activate',
                 resources: {
                     name: 'Resources',
                     title: 'Resources & Activities Monitor',
