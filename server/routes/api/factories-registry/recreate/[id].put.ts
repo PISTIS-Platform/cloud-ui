e4dadffeff1f20e0a570959d2729403fa360a1ed
Giannis Giannopoulos <89904471+Ggiannopoulos@users.noreply.github.com>
2024-09-16 13:25:36 +0300

    Feat: Add recreate services functionality in factory registry (#12)
    * Add recreate services functionality in registry
    
    * Resolve comments

---
+++ server/routes/api/factories-registry/recreate/[id].put.ts
@@ -0,0 +1,15 @@
+import { getToken } from '#auth';
+
+const { factoryRegistryURL } = useRuntimeConfig();
+
+export default defineEventHandler(async (event) => {
+    const token = await getToken({ event });
+    const organizationId = getRouterParam(event, 'id');
+
+    return await $fetch(`${factoryRegistryURL}/factories/recreate/${organizationId}`, {
+        method: 'PUT',
+        headers: {
+            Authorization: `Bearer ${token?.access_token}`,
+        },
+    });
+});
