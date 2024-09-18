6af96c5ceb9c14e7f5657078dcda115bddf3b7c0
Giannis Giannopoulos <giannis.giannopoulos@suite5.eu>
2024-09-18 11:24:48 +0300

    Change balance appearance and calculation

--- pages/wallet.vue
+++ pages/wallet.vue
@@ -1,17 +1,17 @@
 <script lang="ts" setup>
 import dayjs from 'dayjs';
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isBetween from 'dayjs/plugin/isBetween';
 
 import type TableColumn from '~/interfaces/table-column';
 import type { Transaction } from '~/interfaces/wallet-table';
 
-dayjs.extend(isSameOrBefore);
+dayjs.extend(isBetween);
 
 const { t } = useI18n();
 
 //TODO: Change the operation below when we have answer from ICCS/UBI
 /*const { data, error, refresh } = await useLazyFetch<Transaction[]>('/api/wallet');*/
-const data = [
+const data = computed(() => [
     {
         transactionDate: '2024-09-17T13:09:38.901Z',
         transactionId: '1',
@@ -33,22 +33,22 @@ const data = [
         amount: 30,
     },
     {
-        transactionDate: '2024-09-01T13:09:38.901Z',
+        transactionDate: '2024-08-01T13:09:38.901Z',
         transactionId: '5',
         amount: 100,
     },
     {
-        transactionDate: '2024-09-02T13:09:38.901Z',
+        transactionDate: '2024-08-02T13:09:38.901Z',
         transactionId: '6',
         amount: 200,
     },
     {
-        transactionDate: '2024-09-02T13:09:38.901Z',
+        transactionDate: '2024-08-02T13:09:38.901Z',
         transactionId: '7',
         amount: 400,
     },
-];
-const currentDate = new Date();
+]);
+
 const { page, pageCount, filteredRows, paginatedRows, sortBy } = useTable<Transaction[]>(data, 10, {
     column: 'transactionDate',
     direction: 'desc',
@@ -57,31 +57,26 @@ const { page, pageCount, filteredRows, paginatedRows, sortBy } = useTable<Transa
 //TODO: Change the operation below when we have answer from ICCS/UBI
 const currentBalance = /*await useLazyFetch<Balance>('/api/wallet/balance');*/ 3500;
 
-const previousWeekTransactions = data.filter((item) =>
-    dayjs(item.transactionDate).isSameOrBefore(dayjs(currentDate).subtract(1, 'week')),
-);
-
-let pervBalance = 0;
-previousWeekTransactions.forEach((value) => {
-    pervBalance = pervBalance + value.amount;
-});
+const sumAmount = data.value
+    .filter((item) => dayjs(item.transactionDate).isBetween(dayjs().startOf('month'), dayjs(), 'day'))
+    .reduce((total, item) => total + item.amount, 0);
 
 const columns: TableColumn[] = [
     {
         key: 'transactionDate',
         label: t('wallet.date'),
         sortable: true,
-        class: 'text-center w-1/3',
+        class: 'text-start w-1/3',
     },
     {
         key: 'transactionId',
         label: t('wallet.transaction'),
-        class: 'text-center w-1/3',
+        class: 'text-start w-1/3',
     },
     {
         key: 'amount',
         label: t('wallet.amount'),
-        class: 'text-center w-1/3',
+        class: 'text-start w-1/3',
         sortable: true,
     },
 ];
@@ -93,38 +88,25 @@ const columns: TableColumn[] = [
             <div class="flex flex-col w-full">
                 <div class="w-full place-items-stretch pb-4">
                     <div class="relative flex items-center">
-                        <div class="min-w-0 flex-1">
-                            <div class="flex items-center w-full space-x-2">
-                                <div class="flex-shrink-0">
+                        <div class="min-w-0 item-center flex-1">
+                            <div class="flex w-full space-x-2">
+                                <div class="flex-shrink-0 item-center">
                                     <UIcon
                                         name="i-heroicons-currency-dollar-20-solid"
                                         class="w-8 h-8 text-secondary-300"
                                     />
                                 </div>
-                                <h3 class="text-base xl:text-lg font-normal">
+                                <h3 class="text-base xl:text-lg font-normal mt-1">
                                     {{ t('wallet.balance') }}
                                 </h3>
-                                <!-- TODO: need to change the values below when we have the endpoints from wallet -->
-                                <div :class="['text-lg font-bold text-green-800']">
+                                <div class="text-lg mt-1 font-bold text-green-800">
                                     {{ currentBalance }} {{ 'STC' }}
                                 </div>
-                            </div>
-                            <div class="flex items-center w-full space-x-2">
-                                <div class="flex-shrink-0">
-                                    <UIcon
-                                        :name="pervBalance > 0 ? 'i-heroicons-arrow-trending-up' : 'i-heroicons-minus'"
-                                        class="w-8 h-8 text-secondary-300"
-                                    />
+                                <div class="mt-1">
+                                    {{ '(+' }}
+                                    <span class="text-lg font-bold text-green-800"> {{ sumAmount }} </span>
+                                    {{ t('wallet.balanceInMonth') + ')' }}
                                 </div>
-                                <!-- TODO: need to change the values below when we have the endpoints from wallet -->
-                                <div
-                                    :class="['text-lg font-bold', pervBalance > 0 ? 'text-green-800' : 'text-gray-800']"
-                                >
-                                    {{ pervBalance }} {{ 'STC' }}
-                                </div>
-                                <h3 class="text-base xl:text-lg font-normal">
-                                    {{ t('wallet.lastWeek') }}
-                                </h3>
                             </div>
                         </div>
                     </div>
@@ -142,7 +124,9 @@ const columns: TableColumn[] = [
                             }"
                         >
                             <template #transactionDate-data="{ row }">
-                                <span v-if="row.transactionDate">{{ $d(new Date(row.transactionDate), 'short') }}</span>
+                                <span v-if="row.transactionDate" class="item-center">{{
+                                    $d(new Date(row.transactionDate), 'short')
+                                }}</span>
                                 <span v-else>&mdash;</span>
                             </template>
                         </UTable>
