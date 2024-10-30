65314e1f2c0cb7154ba092a002e2e1cc6db8a663
Hermes J. Pappas <hjp@hjp.gr>
2024-10-30 18:02:38 +0200

    Change transactions

--- server/routes/api/wallet/transactions.post.ts
+++ server/routes/api/wallet/transactions.post.ts
@@ -1,16 +1,19 @@
 import { getServerSession, getToken } from '#auth';
 import { walletAliasObject } from '~/constants/walletAliases';
 
+const {
+    public: { marketplaceUrl },
+} = useRuntimeConfig();
+
 //FIXME: Replace with proper service once it's ready
 export default defineEventHandler(async (event) => {
     const session = await getServerSession(event);
     if (!session?.roles?.includes('PISTIS_ADMIN')) return { incoming: [], outgoing: [] };
-    const walletKey = session?.roles?.includes('PISTIS_ADMIN') ? 'pistis-admin' : session?.orgId;
-    if (!walletKey) return { incoming: [], outgoing: [] };
+    const walletKey = 'pistis-admin';
     const body = JSON.stringify({ wallet_alias: walletAliasObject[walletKey].alias });
     const token = await getToken({ event });
 
-    return $fetch<any>(`${walletAlias[walletKey].url}/srv/payments/v0/dlt/transactions`, {
+    return $fetch<any>(`${marketplaceUrl}/srv/payments/v0/dlt/transactions`, {
         method: 'POST',
         body,
         headers: {
