f9d493cdbe46c167bb5f5e8c7d6422015d63dc48
Hermes J. Pappas <hjp@hjp.gr>
2025-02-12 13:28:20 +0200

    Make text no wrap for header

--- pages/transaction-auditor.vue
+++ pages/transaction-auditor.vue
@@ -24,7 +24,7 @@ const columns: TableColumn[] = [
         key: 'amount',
         label: t('auditor.amount'),
         sortable: true,
-        class: 'text-center w-12',
+        class: 'text-center w-12 text-nowrap',
     },
     {
         key: 'assetTitle',
