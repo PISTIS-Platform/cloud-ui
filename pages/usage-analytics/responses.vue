0944e5de350e4fb633347152c398d35c7d669c6c
Hermes J. Pappas <hjp@hjp.gr>
2025-07-16 09:41:09 +0300

    Filter data

--- pages/usage-analytics/responses.vue
+++ pages/usage-analytics/responses.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
-const route = useRoute();
+import type { QuestionResponse } from '~/interfaces/usage-analytics';
 
+const route = useRoute();
 const assetId = computed(() => route.query.assetId);
 
-const { data } = useFetch(`/api/questionnaire/get-answers`, {
+const { data } = useFetch<QuestionResponse[]>(`/api/questionnaire/get-answers`, {
     query: {
         assetId: assetId.value,
     },
 });
+
+const filteredData = computed(() =>
+    data.value ? data.value?.filter((response: QuestionResponse) => response.questionType !== 'Text') : [],
+);
 </script>
 
 <template>
     <div class="justify-center items-center px-8 max-w-7xl mx-auto w-full">
         <PageContainer>
             <div>ID: {{ assetId }}</div>
-            <pre>{{ data }}</pre>
+            <pre>{{ filteredData }}</pre>
         </PageContainer>
     </div>
 </template>
