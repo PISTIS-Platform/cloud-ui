6c077da3b1b64b3ec75c8bc67c3ab4a17abe5680
Hermes J. Pappas <hjp@hjp.gr>
2025-09-12 11:14:26 +0300

    Rename marketplaceUrl to cloudUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
 
     runtimeConfig: {
         public: {
-            marketplaceUrl: '',
+            cloudUrl: '',
         },
         authSecret: '',
         keycloak: {
