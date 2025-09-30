553fb2898e0443a5c2e19b6cd49267dad979c025
Hermes J. Pappas <hjp@hjp.gr>
2025-09-30 11:30:59 +0300

    Add actual number of users and factories to dashboard

--- server/routes/api/dashboard/monitoring-cards.get.ts
+++ server/routes/api/dashboard/monitoring-cards.get.ts
@@ -1,19 +1,37 @@
 import { getServerSession } from '#auth';
+import { getToken } from '#auth';
 import MonitoringCardsData from '~/interfaces/monitoring-cards-data';
+
+const {
+    public: { cloudUrl },
+} = useRuntimeConfig();
 
 export default defineEventHandler(async (event) => {
     const session = await getServerSession(event);
     if (!session?.roles?.includes('PISTIS_ADMIN')) return [];
+
+    const token = await getToken({ event });
+
+    const users = await $fetch<Record<string, any>>(
+        `${cloudUrl}/srv/identity-access-management/api/v1/info/users/count`,
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token?.access_token}`,
+            },
+        },
+    );
+
     const monitoringCards: MonitoringCardsData[] = [
         {
             key: 'factories',
-            amount: 30,
-            change: 2,
+            amount: Object.keys(users.factories).length,
+            change: 0,
         },
         {
             key: 'users',
-            amount: 123,
-            change: 11,
+            amount: users.total,
+            change: 0,
         },
         {
             key: 'assets',
